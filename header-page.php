618dce1bfab60ce19a1c26530a25cde8a42cdfff
Nattapon Jaroenchai <b_nj18@live.com>
2023-03-18 15:45:38 -0500

    fixed conflict flowbite wpform

--- header-page.php
+++ header-page.php
@@ -18,7 +18,7 @@
     <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" /> -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600;700;900&display=swap" rel="stylesheet">
